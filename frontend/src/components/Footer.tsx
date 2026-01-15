ede80308a5da7da20904eb7a51cdf3331d8dc936
churris43 <churris43@hotmail.com>
2026-01-16 09:20:22 +1100

    Removing warning about showing the current year

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,28 @@
+// This has to be client to be able to use new Date.
+// More details https://nextjs.org/docs/messages/next-prerender-current-time-client
+"use client";
+
 import Link from "next/link";
+import { Suspense } from "react";
+
+// This is done
+export function RelativeTime() {
+  return (
+    <span className="mr-1" suppressHydrationWarning>
+      {new Date().getFullYear()}
+    </span>
+  );
+}
 
 function Footer() {
   return (
     <footer className="bg-gray-950 border-t border-gray-800">
       <div className="max-w-7xl mx-auto px-6 py-4 text-center text-sm text-gray-500">
-        © {new Date().getFullYear()} Asset Register. Developed by
+        ©
+        <Suspense fallback={<span>...</span>}>
+          <RelativeTime />
+        </Suspense>
+        Asset Register. Developed by
         <Link
           className="underline ml-1"
           target="_blank"
