bc96defc8e68ee6da4a7895747f1a7f1d10041da
churris43 <churris43@hotmail.com>
2026-01-16 15:22:36 +1100

    Aliases to login to backed and front end containers

---
+++ alias/alias.sh
@@ -0,0 +1,4 @@
+alias d-login-backed="docker exec -it express_api sh"
+alias d-login-frontend="docker exec -it express_webapp sh"
+#alias d-migrate="docker exec -it express_api npx prisma migrate dev"
+#alias d-migrate-status="docker exec -it express_api npm run migrate:status"
