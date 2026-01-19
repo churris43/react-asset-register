5bf18ab0246555cc7a2222469e368bfd7fcc284d
churris43 <churris43@hotmail.com>
2026-01-19 21:25:19 +1100

    Clean up

--- frontend/src/app/roles/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-async function RoleDetails({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <h1 className="text-xl">Details for role {id}</h1>;
-}
-
-export default RoleDetails;
