a63fa77aa6f64c31423c1928028a2dc95be723e8
churris43 <churris43@hotmail.com>
2026-01-20 09:07:40 +1100

    Styling the error page (500)

--- frontend/src/app/global-error.tsx
+++ frontend/src/app/global-error.tsx
@@ -1,6 +1,7 @@
 "use client"; // Error boundaries must be Client Components
 
 import { useEffect } from "react";
+import "./globals.css";
 
 export default function Error({
   error,
@@ -13,11 +14,14 @@ export default function Error({
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
-
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-2xl font-semibold text-white m-2">
+        500: Something went wrong!
+      </h2>
+
       <button
+        className="border-2 px-2 py-2 border-white rounded-md bg-blue-400 hover:bg-blue-600"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
