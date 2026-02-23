08381204949f478405b441f5a00cd1442181318b
churris43 <churris43@hotmail.com>
2026-02-23 15:00:22 +1100

    Add the asset_type field on the asset table

--- db/init.sql
+++ db/init.sql
@@ -8,12 +8,18 @@ CREATE TABLE IF NOT EXISTS `asset` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `asset_name` VARCHAR(255) NOT NULL,
     `role_id` INT NULL,
+    `asset_type_id` INT NULL,
     PRIMARY KEY (`id`),
     INDEX `idx_role_id` (`role_id`),
-    CONSTRAINT `fk_asset_register_role` 
+    CONSTRAINT `fk_asset_role` 
         FOREIGN KEY (`role_id`) 
         REFERENCES `role` (`id`) 
         ON DELETE SET NULL 
+        ON UPDATE CASCADE,
+    CONSTRAINT `fk_asset_asset_type` 
+        FOREIGN KEY (`asset_type_id`) 
+        REFERENCES `asset_Type` (`id`) 
+        ON DELETE SET NULL
         ON UPDATE CASCADE
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
