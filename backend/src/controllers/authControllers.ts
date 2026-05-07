d71cd35a4ab800d18a9e353abc3111e40e89fe86
Arturo AH <mrarturoah@gmail.com>
2026-05-07 15:19:33 +1000

    Fix logout to clear access_token and refresh_token cookies

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -40,6 +40,8 @@ export const login = async (req: Request, res: Response) => {
 };
 
 export const logout = (_req: Request, res: Response) => {
+  res.clearCookie("access_token");
+  res.clearCookie("refresh_token");
   return res.status(200).json({ message: "Logged out" });
 };
 
